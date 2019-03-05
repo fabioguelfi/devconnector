5d51673e44f8400d3282e0f987278a78a45f2c44
fabioguelfi <fabioguelfiunix@gmail.com>
2019-03-05 12:38:46 -0300

    profile fields validation

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -2,6 +2,7 @@ const express = require('express')
 const router = express.Router()
 const mongoose = require('mongoose')
 const passport = require('passport')
+const validateProfileInput = require('../../validation/profile')
 
 // Load Profile Model
 const Profile = require('../../models/Profile')
@@ -29,7 +30,58 @@ router.get('/', passport.authenticate('jwt', { session: false }), async (req, re
         errors.undefined = 'Internal Error'
         res.status(404).json(errors)
     }
+})
 
+// @route POST api/profile
+// @desc Create or Edit user profile
+// @access Private
+router.post('/', passport.authenticate('jwt', { session: false }), async (req, res, nest) => {
+    const { errors, isValid } = validateProfileInput(req.body)
+
+    // Check Validation
+    if (!isValid) {
+        // Return any errors with 400 status
+        return res.status(400).json(errors)
+    }
+
+    // Get fields
+    const profileFields = {}
+    profileFields.user = req.user.id
+    if (req.body.handle) profileFields.handle = req.body.handle
+    if (req.body.status) profileFields.status = req.body.status
+    if (req.body.company) profileFields.company = req.body.company
+    if (req.body.website) profileFields.website = req.body.website
+    if (req.body.location) profileFields.location = req.body.location
+    if (req.body.githubusername) profileFields.githubusername = req.body.githubusername
+    // Skills slipt into array
+    if (typeof req.body.skills !== 'undefined') {
+        profileFields.skills = req.body.skills.split(',')
+    }
+    // Social
+    profileFields.social = {}
+    if (req.body.youtube) profileFields.social.youtube = req.body.youtube
+    if (req.body.twitter) profileFields.social.twitter = req.body.twitter
+    if (req.body.facebook) profileFields.social.facebook = req.body.facebook
+    if (req.body.instagram) profileFields.social.instagram = req.body.instagram
+    if (req.body.linkedin) profileFields.social.linkedin = req.body.linkedin
+
+    const profile = await Profile.findOne({ user: req.user.id })
+    if (profile) {
+        // Update
+        res.json(await Profile.findOneAndUpdate({ user: req.user.id }, { $set: profileFields }, { new: true }))
+    } else {
+        // Create
+
+        // Check if handles exists
+        const profileHandle = await Profile.findOne({ handle: profileFields.handle })
+        if (profileHandle) {
+            errors.handle = 'That handle already exists'
+            res.status(400).json(errors)
+        }
+
+        // Save Profile
+        res.json(await new Profile(profileFields).save())
+    }
 })
 
 module.exports = router
